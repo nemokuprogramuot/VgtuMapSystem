85fc547f0432b8354cff84ac8cd5c79770f9cb0d
Laurynas Butkus <lauris2661@gmail.com>
2024-12-12 21:04:11 +0200

    updaed main

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,23 @@
 import React from "react";
+import mainPhoto from "./photos/mainphoto.png";
 
 const Main = () => {
   return (
     <div style={styles.content}>
       <h2>Patobulintas, lengviau suprantamas</h2>
-      <img
-        src="/path/to/your/map.png" // Update with the correct image path
-        alt="Vilnius Tech Map"
-        style={styles.image}
-      />
+      <div style={styles.imageContainer}> {/* Wrap the image and the button inside a container */}
+        <img
+          src={mainPhoto} // Update with the correct image path
+          alt="Vilnius Tech Map"
+          style={styles.image}
+        />
+        <div
+          // onClick={handleClick}
+          style={styles.button1} // Apply the styles to the button
+        >
+          {/* The content is empty, but the div will still be clickable */}
+        </div>
+      </div>
     </div>
   );
 };
@@ -19,10 +28,25 @@ const styles = {
     padding: "20px",
     textAlign: "center",
   },
+  imageContainer: {
+    position: "relative",  // Make the image container relative
+    display: "inline-block",  // To contain the image and button together
+  },
   image: {
     maxWidth: "100%",
     height: "auto",
     marginTop: "20px",
+  },
+  button1: {
+    position: "absolute", 
+    top: "10%",  
+    left: "10%", 
+    width: "200px",  
+    height: "200px", 
+    backgroundColor: "red", 
+    border: "2px solid blue", 
+    cursor: "pointer", 
+    transition: "background-color 0.3s", 
   },
 };
 
